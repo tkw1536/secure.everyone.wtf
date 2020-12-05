46094683559b04d5ae991ce4a5392e6c912b222e
Tom Wiesing <tkw01536@gmail.com>
2020-12-05 07:04:56 +0100

    Add server-side css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheets } from "@material-ui/core";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -20,4 +21,23 @@ export default class MyDocument extends Document {
       </Html>
     )
   }
+};
+
+MyDocument.getInitialProps = async (ctx) => {
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()],
+  };
 };
